f9ab8e541321500a376bc42528d90f830d7758eb
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-25 01:38:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 class Dog 
-  def name=(dog_name)
-    @name = dog_name
-  end 
+  def initialize(name)
+    @name = name
+  end
   
   def name 
     @name
   end
+  
+  def name=(dog_name)
+    @name = dog_name
+  end 
+  
+  
 end
